911074c7a57cc2ace442b095e9318228cfece2b6
Aaron Gonzales <xysmas@gmail.com>
2015-03-17 16:44:45 -0600

    map works with laplacian and dirchlet

--- src/bayes.py
+++ src/bayes.py
@@ -30,32 +30,47 @@ def calc_priors(train_data):
     w_given_class = phat_word_est(train_data)
 
 
-def phat_word_est(train_data, laplacian=False):
+def phat_word_est(bow_train, class_labels, nclasses=20, alpha=None):
     """Gets P(c|w) for all words in the dictionary.
     Args:
-        train_data (numpy.array): training set
+        bow_train (scipy.sparse): training set, assumes bag of words
+        class_labels(np.array): list of class label names (scikitlearn data.target_names)
+        class_names(list): array of class labels for each document
         laplacian (bool): denote if laplacian is wanted or not
-    Returns: numpy array (float16) of all words and estimates
+        nclasses (int): number of classes in the set
+    Returns: tuple with (numpy array of summed words, numpy array of priors)
     """
-    word_tots = []
-    countsum_words = []
-    # testing on small set
-    # TODO change this
-    for i in range(1, 10000):
-        count = train_data[train_data[:, 1] == i]
-        tot_word = count.sum(axis=0)[2]
-        word_tots.append(tot_word)
-        # print(count)
-        count_tmp = []
-        for i in range(1, 21):
-            # return view over single classes
-            class_view = count[count[:, 3] == i]
-            # gets scalar sum for the counts in the view
-            # and returns just that scalar value; Numpy returns an array
-            class_sum = class_view.sum(axis=0)[2]
-            count_tmp.append(class_sum/tot_word)
-        countsum_words.append(count_tmp)
-    phat_words = np.array(countsum_words, dtype='float16')
-    countsum_words = []
-    word_tots = []
+    vocab_n = bow_train.shape[0]
+    if alpha is not None:
+        # this is done for easy of the for loop's calculation.
+        _alpha = alpha+1
+        denominator_p = alpha*nclasses
+        print('estimating params with Laplacian Smoothing:')
+        print('\t alpha = %d' % alpha)
+        print('\t denominator = %d' % denominator_p)
+    else:
+        # beta is 1/(vocab size)
+        beta = 1/vocab_n
+        _alpha = 1 + beta
+        denominator_p = beta * vocab_n
+        print('estimating params with Dirchlet Prior:')
+        print('\t vocabsize = %f' % vocab_n)
+        print('\t beta = %f' % beta)
+        print('\t alpha = %f' % _alpha)
+        print('\t denominator = %f' % denominator_p)
+
+    # gives the total number of words for a column vector in bow model
+    word_sums = bow_train.sum(axis=0)
+    # preallocate array, 20 row, word dic length
+    phat_words = np.zeros((nclasses, bow_train.shape[1]), dtype='float64')
+
+    # create sum vectors and assign them to the correct spot
+    # may need to fiddle with smoothing params
+    for i in range(nclasses):
+        c_mask = class_labels == i
+        c_sum = bow_train[c_mask].sum(axis=0)
+        c_sum = c_sum + (_alpha - 1)
+        # print(c_sum/word_sums)
+        phat_words[i] = (c_sum / (word_sums + denominator_p))
+    # return (word_sums, phat_words)
     return phat_words
